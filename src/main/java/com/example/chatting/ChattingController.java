25b428d645d80457546634960066a0ac606b252b
JiMinL <youmeyourmy@naver.com>
2024-02-05 13:12:51 +0900

    화면수정

--- src/main/java/com/example/chatting/ChattingController.java
+++ src/main/java/com/example/chatting/ChattingController.java
@@ -1,16 +1,24 @@
 package com.example.chatting;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ChattingController {
+	private final SimpMessagingTemplate messagingTemplate;
 	@GetMapping("/chat")
 	public String chatting() {
 		return "name";
@@ -29,4 +37,15 @@ public class ChattingController {
 		String messageContent = name.getMessageContent();
 		return "[" +nameContent +"]"+ "님: " + messageContent;
 	}
+	
+	@EventListener
+    public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        StompHeaderAccessor headers = StompHeaderAccessor.wrap(event.getMessage());
+
+        // name 객체 생성 및 초기화
+        name name = new name();
+		String nameContent = name.getNameContent();
+        // 연결된 모든 클라이언트에게 입장 알림 메시지를 보냄
+        messagingTemplate.convertAndSend("/topic/greetings", "[" +nameContent +"]"+ "님"+"이 입장하셨습니다.");
+    }
 }
